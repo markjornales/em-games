fa1540d0afb3fd62ac0704c7e1745ebf29a7d44d
RENZ\leore <leorenbamuyanono@gmail.com>
2023-11-08 16:05:50 +0800

    new up today

--- src/app/games/20/Bingo/index.tsx
+++ src/app/games/20/Bingo/index.tsx
@@ -1,6 +1,7 @@
 import CButton from '@/components/CButton'
 import React from 'react'
 import { Group } from 'react-konva'
+import BingoScratch from './BingoScratch'
 
 function Bingo() {
   const scratchCardRef = React.useRef<any>()
@@ -16,6 +17,11 @@ function Bingo() {
   return (
     <Group>
          <CButton label="NEXT CARD" url_path="twentycards" onclickStart={handleButtonMain}  /> 
+         <BingoScratch ref={scratchCardRef} combination={[
+                [false, false, false],
+                [false, true, true],
+              
+            ]}/>
     </Group>
   )
 }
---
+++ src/app/games/50/Bingo/BingoScratch.tsx
@@ -0,0 +1,114 @@
+import { CanvasProvider } from '@/components/CanvasContext';
+import useScratchMethod from '@/hooks/useScratchMethod';
+import useScratchMotion from '@/hooks/useScratchMotion';
+import React from "react";
+import { Group, Image, Rect } from "react-konva"; 
+import PopupAlert from '@/components/PopupAlert'; 
+import Bingo from './Bingo';
+
+type TBingoScratch = {
+    combination: boolean[][]
+}
+type TBingoRef = {
+    isScratchDone: boolean;
+    reset: () => void
+}   
+
+const BingoScratch = React.forwardRef<TBingoRef, TBingoScratch>((props, ref) => {
+    const { combination } = props;
+    const { isCanvasSize } = React.useContext(CanvasProvider);
+    const { height, width } = isCanvasSize;
+    const [isModalShow, setModalshow] = React.useState<boolean>(false);
+    const HEIGHT = height*.75;
+    const WIDTH = width*.86;
+    const x1 = WIDTH*.3;
+    const y1 = HEIGHT*.49;
+    const x2 = WIDTH*.7;
+    const y2 = HEIGHT*.92
+    
+    const {
+        canvas, 
+        isScratchDone, 
+        setScratchDone,
+         setStagePointerPos
+    } = useScratchMethod({HEIGHT, WIDTH, x1, y1, scratchArea: {height: y2-y1, width: x2-x1}, 
+        imageSrc: "/images/50/bingo/front.png"});
+    
+
+    const {
+        handleMouseDown, 
+        handleMouseMove, 
+        handleMouseUp, 
+        handleOnPointerLeave, 
+        imageRef
+    } = useScratchMotion({x1, x2, y1, y2, isScratchDone, setStagePointerPos});
+
+    React.useEffect(() => {
+        if(isScratchDone){ 
+            setModalshow(true);
+        }
+    },[isScratchDone]);
+
+    React.useImperativeHandle(ref, () => ({
+        isScratchDone,
+        reset: () => { 
+            setScratchDone(false);
+            setStagePointerPos([]);
+        },
+    }));
+    
+    return (
+        <Group>
+            <Group x={(width- WIDTH)/2} y={(height-height*.78)/2}>
+
+                <Rect cornerRadius={10} fill="#f0f0f1"width={width*.859} height={HEIGHT}/>
+                {combination.map((data, indexRow) => 
+                    data.map((values, indexColumn) => 
+                    <Group 
+                        opacity={values ? 1: 0.4}
+                        x={WIDTH*(.32 + (0.18 * indexColumn)) } 
+                        y={HEIGHT*(.5 + (0.13 * indexRow))} 
+                        key={indexRow + indexColumn}>
+                        <Bingo imageHeight={WIDTH*.19} assetname='g' imageWidth={WIDTH*.16}/>
+                    </Group>
+                    )
+                )}
+                
+                
+                <Image
+                    ref={imageRef}
+                    image={canvas} 
+                    cornerRadius={10}
+                    onPointerDown={handleMouseDown}
+                    onPointerUp={handleMouseUp}
+                    onPointerMove={handleMouseMove}
+                    onPointerLeave={handleOnPointerLeave}
+                />   
+                
+               
+
+                {/* <Rect 
+                fill="red"
+                width={x2-x1}
+                height={y2-y1}
+                x={x1}
+                y={y1}
+                /> */}
+
+            </Group>
+            <PopupAlert 
+                statusWinner={0}
+                visible={isModalShow}
+                height={height}
+                width={width}
+                onTap={() => {
+                    setModalshow(false);
+                }}
+            />
+        </Group>
+    );
+});
+
+BingoScratch.displayName = "BingoScratch"
+
+export default BingoScratch;
