2f164f0d97a64ac1f1c97ef2cdeee485205d639d
markjoseph <markjornales@gmail.com>
2023-12-03 16:19:17 +0800

    new build puller.py

--- puller.py
+++ puller.py
@@ -4,6 +4,6 @@ from colorama import Fore, Style
 enter_commit = input(f"{Fore.GREEN} Enter your messenge commit {Style.RESET_ALL}: ")
 os.system("git pull dev_emgames master")
 os.system("git add .")
-os.system(f"git commit -m {enter_commit}")
+os.system(f'git commit -m "{enter_commit}"')
 os.system("git push dev_emgames master")
 os.system("git push prod_emgame master")
