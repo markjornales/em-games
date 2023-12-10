674069a2ad2355bd6d29758c20c9dd6a5b29f4d9
markjoseph <markjornales@gmail.com>
2023-12-11 00:31:36 +0800

    deployment script added

--- deploy.py
+++ deploy.py
@@ -14,3 +14,5 @@ for cmd in split_str:
 os.system("git pull origin master")
 os.system("docker build -t em-game .")
 os.system("docker run -d -p 89:89 em-game")
+os.system("docker ps")
+print(f"\nSuccess Deployment!....")
