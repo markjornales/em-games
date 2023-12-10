d55e8d817284daf2cdaf070ae2d01747327574b5
markjoseph <markjornales@gmail.com>
2023-12-11 00:36:34 +0800

    changes for deploy.py script

--- deploy.py
+++ deploy.py
@@ -9,8 +9,9 @@ for cmd in split_str:
     regex_pattern = r"\b[0-9a-fA-F]{12}\b"
     match_pattern = re.findall(regex_pattern, cmd)
     if len(match_pattern) > 0: 
+        print("stop docker id: ")
         os.system(f"docker stop {match_pattern[0]}")
-    
+
 os.system("git pull origin master")
 os.system("docker build -t em-game .")
 os.system("docker run -d -p 89:89 em-game")
